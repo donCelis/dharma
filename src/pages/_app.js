53f0b37932c0535089e5654872208e75ae97118b
Alexander Celis <alexcelis019@gmail.com>
2022-07-21 17:28:20 -0500

    add: loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'nprogress/nprogress.css';
 import '../styles/globals.scss';
 
 import Head from 'next/head';
 import { AppProvider } from 'src/context';
+import { Router } from 'next/router';
+import nProgress from 'nprogress';
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
